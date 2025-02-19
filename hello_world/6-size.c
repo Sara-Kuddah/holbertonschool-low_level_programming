b3530eb5a95a5cc3d3ab3336ad0fdc5d4b844a5d
Sara-Kuddah <sara-kuddah@hotmail.com>
2025-02-19 15:03:53 +0300

    test6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
-  char c;
-  int i;
-  long l;
-  long long l2;
-  float f;
+	char c;
+	int i;
+	long li;
+	long long lii;
+	float f;
 
-  printf("Size of a char: %ld byte(s)",sizeOf(c));
-  printf("Size of an int: %d byte(s)", sizeOf(i));
-  printf("Size of a long int: 4 byte(s)" , sizeOf(l));
-  printf("Size of a long long int: 8 byte(s)" , sizeOf(l2));
-  printf("Size of a float: 4 byte(s)" , sizeOf(f));
-
-  return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lii));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
 }
