dd755c12184896ef312f90801b08fee67c094e00
Sara-Kuddah <sara-kuddah@hotmail.com>
2025-02-19 14:46:04 +0300

    test 4567

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio>
+
+int main(void)
+{
+  char c;
+  int i;
+  long l;
+  long long l2;
+  float f;
+
+  printf("Size of a char: %ld byte(s)",sizeOf(c));
+  printf("Size of an int: %d byte(s)", sizeOf(i));
+  printf("Size of a long int: 4 byte(s)" , sizeOf(l));
+  printf("Size of a long long int: 8 byte(s)" , sizeOf(l2));
+  printf("Size of a float: 4 byte(s)" , sizeOf(f));
+
+  return (0);
+}
